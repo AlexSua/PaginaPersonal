64f372e5f2317c45c81d5e496f5206366d50315a
AlexSua <8196195+AlexSua@users.noreply.github.com>
2019-11-11 21:49:17 +0100

    Fickering fix 2.0

--- js/main.js
+++ js/main.js
@@ -109,21 +109,17 @@ function animacionTrabajo() {
                   false
                 );
                 cronograma_contenido.style.display = "none";
-                top = elemento.getBoundingClientRect().top + window.scrollY;
-                console.log(top)
-                window.scrollTo({ top: top, left: 0 });
                 Array.from(elementos_trabajo).forEach(el => {
                   if (el != elemento) {
                     el.style.display = "";
-                   
                   }
                 });
-               
                 cronograma_fecha_div.style.width = "";
               }
               document.body.style.overflow = "auto";
               top = elemento.getBoundingClientRect().top + window.scrollY;
-              console.log(top)
+              window.requestAnimationFrame(animationFrame);
+
               window.scrollTo({ top: top, left: 0 });
             },
             false
